92c5167d1e00185e7b43edae559a67a8abb8220c
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2024-03-06 13:25:15 -0800

    save.

--- server.go
+++ server.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
+	"wohlburger.io/layout"
 )
+
+func Render(ctx echo.Context, statusCode int, t templ.Component) error {
+	ctx.Response().Writer.WriteHeader(statusCode)
+	ctx.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
+	return t.Render(ctx.Request().Context(), ctx.Response().Writer)
+}
 
 func main() {
 	e := echo.New()
+	fmt.Println("hello creature ...")
 
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "hello creature ...")
+		return Render(c, http.StatusOK, layout.MainLayout("Chris"))
 	})
 
 	e.Start(":8080")
